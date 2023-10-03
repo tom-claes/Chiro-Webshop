cde3f222e70b484908e679f77ce94e9ccb04f391
tom-claes <133092129+tom-claes@users.noreply.github.com>
2023-10-03 11:43:07 +0200

    Commit 3
    Nodige tabellen en modellen aangemaakt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,4 +13,26 @@ class UserController extends Controller
         return view('site.user', ['user' => $user]);
     }
 
+    public function update()
+    {
+        $request->validate([
+            'bio' => ['required', 'string', 'max:1000'],
+            'residence' => ['required', 'string', 'max:85'],
+            'language' => ['required', 'max:50'],
+            'pet' => ['string', 'max:42'],
+            'hobby' => ['string', 'max:50'],
+            'interest' => ['string', 'max:255'],
+            'toy' => ['string', 'max:255'],
+            'food' => ['string', 'max:255'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'firstname' => $request->firstname,
+            'gender' => $request->gender,
+            'birthdate' => $request->birthdate,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+    }
 }
